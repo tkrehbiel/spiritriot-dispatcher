4abd86aa00baa4642967ed4d7a078d8883c1bb55
Thomas Krehbiel <tkrehbiel@gmail.com>
2023-12-09 18:10:27 -0500

    wording

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env.local' });
 
 async function process(message) {
-  console.log('processing new post:', JSON.stringify(message));
+  console.log('dispatching work for new post:', JSON.stringify(message));
 }
 
 export async function handler(event, context) {
