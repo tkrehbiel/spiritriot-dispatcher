5f3839103bfd79185fe4823d852a3621d81cefbf
Thomas Krehbiel <tkrehbiel@gmail.com>
2023-12-09 21:05:24 -0500

    add sending output queues

--- index.mjs
+++ index.mjs
@@ -1,9 +1,29 @@
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
+import { fromNodeProviderChain } from '@aws-sdk/credential-providers';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env.local' });
 
+const sqsClient = new SQSClient({
+  region: process.env.AWS_REGION,
+  credentials: fromNodeProviderChain(),
+});
+
+async function sendMessage(queueName, message) {
+  if (!queueName) return;
+  const command = new SendMessageCommand({
+    QueueUrl: queueName,
+    MessageBody: JSON.stringify(message),
+  });
+  return sqsClient.send(command);
+}
+
 async function process(message) {
   console.log('dispatching work for new post:', JSON.stringify(message));
+  const promises = [];
+  promises.push(sendMessage(process.env.NOTIFIER_QUEUE));
+  promises.push(sendMessage(process.env.WEBMENTION_QUEUE));
+  await Promise.all(promises);
 }
 
 export async function handler(event, context) {
