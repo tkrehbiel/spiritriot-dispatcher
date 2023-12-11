a8af8a9178dd481075fb0e4946f8031a73306e70
Thomas Krehbiel <tkrehbiel@gmail.com>
2023-12-11 15:01:24 -0500

    ugh goodbye node.js

--- __tests__/baseline.test.mjs
+++
@@ -1,5 +0,0 @@
-describe('test suite', () => {
-    test('mock index', () => {
-      console.log('test test');
-    })
-});
--- jest.config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-  verbose: true,
-  testMatch: ['**/__tests__/**/*.mjs?(x)'],
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.mjs$': 'babel-jest',
-  },
-};
